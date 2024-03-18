b3de6fc42e3f001bf8754b2750191ccadaeaebb4
Edmound <eddnjoori@gmail.com>
2024-03-18 09:19:27 +0300

    Adds a js prompt on load page

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function welcomeTo(){
+    const useName=prompt('What is your name?');
+
+    console.log("Usename");
+
+    const greetings=`${useName} `+"Welcome to Edmounds' portfolio. Hope for you to have the best experience.";
+    console.log("greetings");
+
+    alert(greetings);
+}
+
