3186c7bf0c031ca9300e18a1e56fffca59ce1fdb
Edmound <eddnjoori@gmail.com>
2024-03-19 10:05:25 +0300

    Adds js script

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-function welcomeTo(){
+function welcomeToPortfolio(){
     const useName=prompt('What is your name?');
 
     console.log("Usename");
 
-    const greetings=`${useName} `+"Welcome to Edmounds' portfolio. Hope for you to have the best experience.";
+    const greetings=`${useName} `+",welcome to Edmounds' portfolio. Hope for you to have the best experience.";
     console.log("greetings");
 
     alert(greetings);
